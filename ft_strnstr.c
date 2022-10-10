aa7c863cc66c6b139672cdad81502924877cae6d
Ilias Asrarfi <ilasrarf@e2r5p10.1337.ma>
2022-10-10 17:55:23 +0100

    last ap

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: ilasrarf <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 19:39:34 by ilasrarf          #+#    #+#             */
-/*   Updated: 2022/10/10 17:09:34 by ilasrarf         ###   ########.fr       */
+/*   Updated: 2022/10/10 17:54:13 by ilasrarf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,23 @@ char	*ft_strnstr(const char *s, const char *find, size_t len)
 	
 	i = 0;
 	len_f = ft_strlen(find);
+	if (len == 0)
+		return (NULL);
 	while (s[i])
 	{
 		if(ft_strncmp((s + i), find, len_f) == 0)
-			return;
+			return (s + i);
+		i++;
 	}
+	return (s);
 }
 
 int main(int argc, char const *argv[])
 {
-	ft_strnstr("12345678", "456", 8);
+
+	char *s1 = "";
+	char *s2 = "";
+	printf("%s", ft_strnstr(s1, s2, 0));
+	printf("%s", strnstr(s1, s2, 0));
 	return 0;
 }
